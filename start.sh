fed6fd2de3395c1081c2aef7dd7918307fd1c30e
Pavel Shershnev <pvutrix@gmail.com>
2025-12-06 04:31:55 +0500

    fix(deployment): run typeorm migrations on startup

--- start.sh
+++ start.sh
@@ -3,5 +3,10 @@
 # Start nginx
 nginx -g 'daemon off;' &
 
+# Run migrations explicitly
+echo "Running migrations..."
+./node_modules/.bin/typeorm migration:run -d ./dist/packages/backend/config/typeorm.js
+echo "Migrations completed (exit code $?)"
+
 # Start all applications defined in ecosystem.config.js
 pm2-runtime ecosystem.config.js
