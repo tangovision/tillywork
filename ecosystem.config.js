3849b74508058ece650dc7c51ed016ce51bed5dd
Pavel Shershnev <pvutrix@gmail.com>
2025-12-06 03:36:17 +0500

    fix(deployment): correct backend script path in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'backend',
-            script: './dist/backend/main.js',
+            script: './dist/packages/backend/main.js',
             exec_mode: 'cluster',
             instances: 1, // Adjust the number of instances according to your needs
         }
