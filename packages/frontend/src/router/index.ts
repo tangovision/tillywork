a22fab5e372f89e0e9f9e53bea366559241fbb53
Pavel Shershnev <pvutrix@gmail.com>
2025-12-06 02:46:04 +0500

    fix(frontend): explicitly inject value-router routes to prevent startup error

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router/auto';
+import { routes } from 'vue-router/auto-routes';
 import { requireAuthGuard, requireGuestGuard } from './guards';
 
 // Create the router instance
 const router = createRouter({
   history: createWebHistory(import.meta.env.TW_BASE_URL), // Using HTML5 history mode
+  routes,
   scrollBehavior() {
     // always scroll to top
     return { top: 0 };
